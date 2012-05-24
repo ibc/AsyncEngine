51a12dc47de722743254b4cc2c8f4d3b519174fa
Iñaki Baz Castillo <ibc@aliax.net>
2012-05-24 14:18:20 +0200

    Method renamed.

--- lib/asyncengine/async.rb
+++ lib/asyncengine/async.rb
@@ -1,7 +1,7 @@
 module AsyncEngine
 
   def self.call_from_other_thread pr=nil, &bl
-    ensure_AE_is_ready_for_handles()
+    ensure_ready_for_handles()
 
     _c_call_from_other_thread pr || bl
   end
